fa3a4cc304e91c81c9195f0a29e83f19d4fe70cf
Cross Z <xin.zheng@take2.health>
2022-08-05 15:24:08 +0800

    :tada: proper asgi.py settings from official support.

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -8,48 +8,33 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-# application = get_asgi_application()
+asgi_application = get_asgi_application()
 
 
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.http import AsgiHandler
+# from channels.http import AsgiHandler
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django_nextjs.proxy import NextJSProxyHttpConsumer, NextJSProxyWebsocketConsumer
 
 from django.conf import settings
-from django.urls import path
+from django.urls import path, re_path
 
 # http_routes = []
+http_routes = [re_path(r"", asgi_application)]
 websocket_routers = []
 
 if settings.DEBUG:
-    # http_routes.insert(0, re_path(r"^(?:_next|__next|next).*", NextJSProxyHttpConsumer.as_asgi()))
+    http_routes.insert(0, re_path(r"^(?:_next|__next|next).*", NextJSProxyHttpConsumer.as_asgi()))
     websocket_routers.insert(0, path("_next/webpack-hmr", NextJSProxyWebsocketConsumer.as_asgi()))
 
-
 application = ProtocolTypeRouter({
-  "http": AsgiHandler(),
+  # "http": AsgiHandler(),
+  "http": URLRouter(http_routes),
   ## Just HTTP for now. (We can add other protocols later.)
   "websocket": AuthMiddlewareStack(URLRouter(websocket_routers)),
 })
-
-
-
-
-# http_routes = [path("", django_asgi_app)]
-
-# application = ProtocolTypeRouter(
-#     {
-#         # Django's ASGI application to handle traditional HTTP and websocket requests.
-#         "http": URLRouter(http_routes),
-#         "websocket": AuthMiddlewareStack(URLRouter(websocket_routers)),
-#         # ...
-#     }
-# )
